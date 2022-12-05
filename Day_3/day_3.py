162af4c50ce0a75d5e13d6175a6cdc6e29228635
gabrieljlapolla <gabrieljlapolla@protonmail.com>
2022-12-04 23:33:16 -0500

    Complete part two

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -1,17 +1,12 @@
 # Advent of Code 2022 - Day 3
 
 file_name = 'day_3_input.txt'
-input = open(file_name, 'r') # Open file
-lines = input.readlines() # Read in all lines
-lines = [game.strip() for game in lines] # Remove whitespace
+lines = []
 
-test = ['vJrwpWtwJgWrhcsFMMfFFhFp',
-        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-        'PmmdzqPrVvPwwTWBwg',
-        'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-        'ttgJtRGJQctTZtZT',
-        'CrZsJsPPZsGzwwsLwLmpwMDw']
-#lines = test
+with open(file_name, 'r') as input:
+    lines = input.readlines() # Read in all lines
+
+lines = [game.strip() for game in lines] # Remove whitespace
 
 sum = 0
 for line in lines:
@@ -21,9 +16,25 @@ for line in lines:
     for char in first_half: # Check chars in first half
         if char in last_half: # Compare chars to last half
             if char.islower():
-                sum += ord(char) - 96
+                sum += ord(char) - 96 # Convert letter to value
             else:
                 sum += ord(char) - 38
             break # Match is found so don't check more
 
 print(f'Sum: {sum}')
+
+# --- Part Two ---
+p_sum = 0
+for i in range(0, len(lines), 3): # Iterate by grous of 3 lines
+    line_1 = lines[i]
+    line_2 = lines[i + 1]
+    line_3 = lines[i + 2]
+    for char in line_1:
+        if char in line_2 and char in line_3: # If all three contain the same letter
+            if char.islower():
+                p_sum += ord(char) - 96 # Convert letter to value
+            else:
+                p_sum += ord(char) - 38
+            break # Match is found so don't check more
+
+print(f'Priorities sum: {p_sum}')
